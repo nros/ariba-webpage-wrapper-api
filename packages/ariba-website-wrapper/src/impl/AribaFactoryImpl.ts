a6d1d4aea0a591726ba3deaa71a8b46ed0e58fa4
Nikolaus Rosenmayr <nr@firecoder.com>
2021-12-17 20:03:11 +0100

    chore(style): make linter happy
    Signed-off-by: Nikolaus Rosenmayr <nr@firecoder.com>

--- packages/ariba-website-wrapper/src/impl/AribaFactoryImpl.ts
+++ packages/ariba-website-wrapper/src/impl/AribaFactoryImpl.ts
@@ -108,7 +108,7 @@ export class AribaFactoryImpl implements IAribaFactory {
 
                 // setting a specific user agent to speed up headless mode
                 // see: https://github.com/puppeteer/puppeteer/issues/1718
-                await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36')
+                await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36");
 
                 await page.setViewport(this.getViewportSize(this._config));
 
